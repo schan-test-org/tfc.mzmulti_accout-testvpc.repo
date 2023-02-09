063a08b79873a30149b64ef645faa306efc0a97e
schan90 <qnas90@gmail.com>
2023-02-09 12:06:37 +0900

    6th

--- __tfwk-conf.tf
+++ __tfwk-conf.tf
@@ -27,9 +27,8 @@ terraform {
 provider "aws" {
   region  = var.aws_region
   # profile = var.aws_profile
-  access_key = var.dev_access_key
-  secret_key = var.dev_secret_key
-
+  access_key = try(var.env, "") == "dev" ? var.dev_access_key : var.qa_access_key
+  secret_key = try(var.env, "") == "dev" ? var.dev_secret_key : var.dev_secret_key
   ignore_tags {
     key_prefixes = ["kubernetes.io", "k8s.io"]
   }
